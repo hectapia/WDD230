bec2d73aa1b803e549cceb5107a477a60b62f66f
comimsaurs <holivares@comimsa.com>
2023-01-27 11:25:22 -0600

    Chamber site

---
+++ chamber/scripts/scripting.js
@@ -0,0 +1,32 @@
+function toggleMenu() {
+  document.getElementById("primaryNav").classList.toggle("open");
+  document.getElementById("hamburgerBtn").classList.toggle("open");
+}
+const x = document.getElementById("hamburgerBtn");
+x.onclick = toggleMenu;
+
+
+// select the elements to manipulate (output to)
+const datefield = document.querySelector(".date");
+//const datefieldUK = document.querySelector("aside"); // for european/family history format with day first.
+
+// derive the current date using a date object
+const now = new Date();
+const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
+	now);
+const fulldateUK = new Intl.DateTimeFormat("en-UK", {
+	dateStyle: "full" }).format(now);
+// long, medium, short options ... try them
+
+
+
+datefield.innerHTML = `<em>${fulldateUK}</em>`;
+//datefieldUK.innerHTML = `<em>${fulldateUK}</em>`;
+
+
+
+
+let date = new Date();
+let year = date.getFullYear();
+document.getElementById('currentyear').innerHTML = year;
+document.getElementById('lastupdated').textContent = `Last Updated: ${document.lastModified}`;
