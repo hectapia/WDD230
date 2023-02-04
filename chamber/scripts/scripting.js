7f046a7c1db6d76b66672c1e179f5cda1f2905a5
Hector Olivares Tapia <97076427+hectapia@users.noreply.github.com>
2023-02-04 17:08:27 -0600

    Update scripting.js

--- chamber/scripts/scripting.js
+++ chamber/scripts/scripting.js
@@ -7,15 +7,15 @@ x.onclick = toggleMenu;
 
 
 // select the elements to manipulate (output to)
-const datefield = document.querySelector(".date");
+//const datefield = document.querySelector(".date");
 //const datefieldUK = document.querySelector("aside"); // for european/family history format with day first.
 
 // derive the current date using a date object
-const now = new Date();
-const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
-	now);
-const fulldateUK = new Intl.DateTimeFormat("en-UK", {
-	dateStyle: "full" }).format(now);
+//const now = new Date();
+//const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
+//	now);
+//const fulldateUK = new Intl.DateTimeFormat("en-UK", {
+//	dateStyle: "full" }).format(now);
 // long, medium, short options ... try them
 
 
@@ -24,6 +24,18 @@ datefield.innerHTML = `<em>${fulldateUK}</em>`;
 //datefieldUK.innerHTML = `<em>${fulldateUK}</em>`;
 
 
+// Header date manipulation
+const datefield = document.querySelector(".date");
+const now = new Date();
+const meetday = now.getDay();
+//const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(now);
+//datefield.innerHTML = `<em>${fulldate}</em>`;
+const fulldateUK = new Intl.DateTimeFormat("en-UK", {	dateStyle: "full" }).format(now);
+datefield.innerHTML = `<em>${fulldateUK}</em>`;
+  
+if (meetday == 6) { 
+    chambermeet.innerHTML = `<p>🤝🏼 Come join us for the chamber meet and greet Wednesday at 7:00 p.m.</p>`;
+}
 
 
 let date = new Date();
