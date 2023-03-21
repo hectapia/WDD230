dec5cb1b870d9f3532460084462f4a838622ed9c
hectapia <oli21007@byui.edu>
2023-03-21 00:45:54 -0600

    Discover page updated

--- chamber/scripts/scripting.js
+++ chamber/scripts/scripting.js
@@ -1,3 +1,21 @@
+// Header date manipulation
+const dateField = document.querySelector(".date");
+const now = new Date();
+const numberDay = now.getDay();
+const fulldateUK = new Intl.DateTimeFormat("en-UK", {	dateStyle: "full" }).format(now);
+dateField.innerHTML = `<em>${fulldateUK}</em>`;
+
+// A banner between days in the header
+var banner = document.querySelector(".banner");
+if (numberDay == 1 || numberDay == 2) { 
+    banner.style.display = 'block';
+    banner.innerHTML = `<p>🤝🏼 Come join us for the chamber meet and greet Wednesday at 7:00 p.m.</p>`;
+} else {
+  banner.style.display = 'none';
+}
+
+
+// Toggling between primary navigation and hamburger menu navigation
 function toggleMenu() {
   document.getElementById("primaryNav").classList.toggle("open");
   document.getElementById("hamburgerBtn").classList.toggle("open");
@@ -6,25 +24,19 @@ const x = document.getElementById("hamburgerBtn");
 x.onclick = toggleMenu;
 
 
-// Header date manipulation
-const datefield = document.querySelector(".date");
-const now = new Date();
-const meetday = now.getDay();
-//const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(now);
-//datefield.innerHTML = `<em>${fulldate}</em>`;
-const fulldateUK = new Intl.DateTimeFormat("en-UK", {	dateStyle: "full" }).format(now);
-datefield.innerHTML = `<em>${fulldateUK}</em>`;
+// Join page: Value of hidden input
+const lastFormSubmit = document.getElementById('lastFormSubmit');
+if (lastFormSubmit) lastFormSubmit.value = new Date();
 
-var banner = document.querySelector(".banner");
-if (meetday == 1 || meetday == 2) { 
-    banner.style.display = 'block';
-    banner.innerHTML = `<p>🤝🏼 Come join us for the chamber meet and greet Wednesday at 7:00 p.m.</p>`;
-} else {
-  banner.style.display = 'none';
-}
-
-
+// Last Updated
 let date = new Date();
 let year = date.getFullYear();
 document.getElementById('currentyear').innerHTML = year;
 document.getElementById('lastupdated').textContent = `Last Updated: ${document.lastModified}`;
+
+
+// notes:
+
+// Header date manipulation variation: Intl.DateTimeFormat
+//const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(now);
+//dateField.innerHTML = `<em>${fulldate}</em>`;
